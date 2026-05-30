e77dd5cebdb4411b53ae43d287460e6c2ee9baff
PragatiPatro <pragatikumaripatro@gmail.com>
2026-05-30 15:19:41 +0530

    the copy fun

---
+++ q7.kt
@@ -0,0 +1,12 @@
+data class User(
+    val id: Int,
+    val username: String,
+    val isActive: Boolean
+)
+
+fun main() {
+    val user1 = User(1, "Pragati", true)
+    val user2 = user1.copy(isActive = false)
+    println(user1)
+    println(user2)
+}
