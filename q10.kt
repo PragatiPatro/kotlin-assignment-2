f735b045593c2c48a9180a21c3b15b8da5f3c43b
PragatiPatro <pragatikumaripatro@gmail.com>
2026-05-30 20:53:38 +0530

    sealed class

---
+++ q10.kt
@@ -0,0 +1,9 @@
+sealed class ScreenState {
+    object Loading : ScreenState()
+    data class Success(val data: String) : ScreenState()
+}
+
+fun main() {
+    val state: ScreenState = ScreenState.Success("Data Loaded")
+    println(state)
+}
