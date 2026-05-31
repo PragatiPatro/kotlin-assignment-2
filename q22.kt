2aed3344d74f03baf6f1e4160d3ca16fe2bc03d9
PragatiPatro <pragatikumaripatro@gmail.com>
2026-05-31 20:28:04 +0530

    exhaustive state machine

---
+++ q22.kt
@@ -0,0 +1,24 @@
+sealed class ScreenState {
+    object Loading : ScreenState()
+    data class Success(val data: String) : ScreenState()
+    data class Error(val errorMsg: String) : ScreenState()
+}
+
+fun renderUI(state: ScreenState) {
+    when (state) {
+        is ScreenState.Loading ->
+            println("Loading data...")
+
+        is ScreenState.Success ->
+            println("Success: ${state.data}")
+
+        is ScreenState.Error ->
+            println("Error: ${state.errorMsg}")
+    }
+}
+
+fun main() {
+    renderUI(ScreenState.Loading)
+    renderUI(ScreenState.Success("Data Loaded"))
+    renderUI(ScreenState.Error("Network Error"))
+}
